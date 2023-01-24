b29a73bdbf28f998639780079b6051b1541e8362
cvanetten <cvanetten@trek10.com>
2023-01-24 14:00:13 -0500

    adding structure

--- RenameToolPackage/setup.py
+++ RenameToolPackage/setup.py
@@ -5,5 +5,9 @@ setuptools.setup(
   version="0.0.1",
   author="Connor Van Etten",
   description="This PyPi is a small command line extension to help rename singular and mulitple files using python",
-  packages=["RenameTool"]
+  py_modules=['yourscript'],
+  install_requires=['Click',],
+  entry_points={
+    'console_scripts': ['renametool = renametool:cli',],
+    },
 )
